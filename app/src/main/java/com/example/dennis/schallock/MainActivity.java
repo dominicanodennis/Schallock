dc4fd68047926310a3e2795be95ad8b3cea5d1ab
dominicanodennis <dominicanodennis@hotmail.de>
2015-03-14 16:34:33 +0100

    neuer commit

--- app/src/main/java/com/example/dennis/schallock/MainActivity.java
+++ app/src/main/java/com/example/dennis/schallock/MainActivity.java
@@ -1,17 +1,47 @@
 package com.example.dennis.schallock;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    String hallo = "dennis";
+    int number;
+    ListView listview;
+    Button button;
+    TextView textView2;
+    Spinner spinner1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView2 = (TextView) findViewById(R.id.textView2);
+        textView2.setText("Motherfucker");
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void restart() {
+        finish();
     }
 
 
@@ -31,6 +61,7 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            restart();
             return true;
         }
 
